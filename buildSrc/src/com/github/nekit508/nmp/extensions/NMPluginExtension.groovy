f897a28085fc8969077770d146133456556c26a0
nekit508 <lastp508@gmail.com>
2025-09-05 23:47:02 +0300

    core extension docs fix

--- buildSrc/src/com/github/nekit508/nmp/extensions/NMPluginExtension.groovy
+++ buildSrc/src/com/github/nekit508/nmp/extensions/NMPluginExtension.groovy
@@ -36,7 +36,7 @@ abstract class NMPluginExtension {
 
     @Deprecated(forRemoval = true)
     protected void settingsI(Closure cl) {
-        nmp.setting () -> ConfigureUtil.configureSelf cl, this
+        ConfigureUtil.configureSelf cl, this
     }
 
     @Deprecated(forRemoval = true)
@@ -44,14 +44,14 @@ abstract class NMPluginExtension {
 
     @Deprecated(forRemoval = true)
     protected void configureI(Closure cl) {
-        nmp.configuration () -> ConfigureUtil.configureSelf cl, this
+        ConfigureUtil.configureSelf cl, this
     }
 
     @Deprecated(forRemoval = true)
     abstract <T> T configure(Closure closure)
 
     void configureTasks(@DelegatesTo(TaskContainer) Closure closure) {
-        nmp.setting () -> ConfigureUtil.configureSelf closure, attachedProject.tasks
+        nmp.configuration { ConfigureUtil.configureSelf closure, attachedProject.tasks }
     }
 
     /** There you can initialize all your properties. */
